642c23d1bb1f22449c2878ff837d56c3b1772dea
Achinta Hazra <achintahazra8515@gmail.com>
2026-04-18 22:02:17 +0530

    sdvd

--- ADMIN/app/(protected)/page.js
+++ ADMIN/app/(protected)/page.js
@@ -98,7 +98,7 @@ export default function HomePage() {
               <div className="relative">
                 <Shield className="w-16 h-16 text-cyan-400 animate-pulse" />
                 <Zap className="w-8 h-8 text-indigo-400 absolute -bottom-1 -right-1" />
-              </div>
+              </div> 
             </div>
 
             <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-white via-cyan-200 to-indigo-200 bg-clip-text text-transparent">
